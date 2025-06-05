de6dacbb071f661f92d40a0a0facdc7c805678f7
PavloKhrm <pavlo.kharaman@student.nhlstenden.com>
2025-06-05 16:20:20 +0200

    Restored Artioms work

---
+++ models/Place.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  AppDevelopment
+//
+//  Created by M1stake Sequence on 2025-06-05.
+//
+
+import CoreLocation
+
+struct Place: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+    let placeIcon: String
+    var isCaptured: Bool = false
+}
